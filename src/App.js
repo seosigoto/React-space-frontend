1c56c7b1200eadfd85c96639ef146b109e163ada
Seo <seo.sigoto@gmail.com>
2022-01-28 13:53:29 -0800

    front end to ether.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 import twitterLogo from './assets/twitter-logo.svg';
+import SelectCharacter from './Components/SelectCharacter';
+import { CONTRACT_ADDRESS, transformCharacterData } from './constants';
+import myEpicGame from './utils/MyEpicGame.json';
+import Arena from './Components/Arena';
+import LoadingIndicator from './Components/LoadingIndicator';
 import './App.css';
 
 // Constants
@@ -7,30 +13,181 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
-          <p className="sub-text">Team up to protect the Metaverse!</p>
+  /*
+   * Just a state variable we use to store our user's public wallet. Don't forget to import useState.
+   */
+  // State
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [characterNFT, setCharacterNFT] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  /*
+   * Start by creating a new action that we will run on component load
+   */
+  // Actions
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log('Make sure you have MetaMask!');
+        return;
+      } else {
+        console.log('We have the ethereum object', ethereum);
+
+        /*
+         * Check if we're authorized to access the user's wallet
+         */
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+        /*
+         * User can have multiple authorized accounts, we grab the first one if its there!
+         */
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log('Found an authorized account:', account);
+          setCurrentAccount(account);
+        } else {
+          console.log('No authorized account found');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    setIsLoading(false);
+  };
+
+  const checkNetwork = async () => {
+    try {
+      const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+      if (chainId != 4 ) {
+        alert("Please connect to Rinkeby!")
+      }
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  // Render Methods
+  const renderContent = () => {
+    if (isLoading) {
+      return <LoadingIndicator />;
+    }
+    /*
+     * Scenario #1
+     */
+    if (!currentAccount) {
+      return (
           <div className="connect-wallet-container">
             <img
-              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
-              alt="Monty Python Gif"
+                src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
+                alt="Monty Python Gif"
             />
+            <button
+                className="cta-button connect-wallet-button"
+                onClick={connectWalletAction}
+            >
+              Connect Wallet To Get Started
+            </button>
+          </div>
+      );
+      /*
+       * Scenario #2
+       */
+    } else if (currentAccount && !characterNFT) {
+      return <SelectCharacter setCharacterNFT={setCharacterNFT} />;
+    } else if (currentAccount && characterNFT) {
+      return <Arena characterNFT={characterNFT} setCharacterNFT={setCharacterNFT} />;
+    }
+  };
+
+   const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert('Get MetaMask!');
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+    /*
+     * This runs our function when the page loads.
+     */
+  useEffect(() => {
+    setIsLoading(true);
+    checkIfWalletIsConnected();
+    checkNetwork();
+  }, []);
+
+  useEffect(() => {
+    /*
+     * The function we will call that interacts with out smart contract
+     */
+    const fetchNFTMetadata = async () => {
+      console.log('Checking for Character NFT on address:', currentAccount);
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const gameContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicGame.abi,
+          signer
+      );
+
+      const txn = await gameContract.checkIfUserHasNFT();
+      if (txn.name) {
+        console.log('User has character NFT');
+        setCharacterNFT(transformCharacterData(txn));
+      } else {
+        console.log('No character NFT found');
+      }
+
+      setIsLoading(false);
+    };
+
+    /*
+     * We only want to run this, if we have a connected wallet
+     */
+    if (currentAccount) {
+      console.log('CurrentAccount:', currentAccount);
+      fetchNFTMetadata();
+    }
+  }, [currentAccount]);
+
+  return (
+      <div className="App">
+        <div className="container">
+          <div className="header-container">
+            <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
+            <p className="sub-text">Team up to protect the Metaverse!</p>
+            {/* This is where our button and image code used to be!
+             *	Remember we moved it into the render method.
+             */}
+            {renderContent()}
+          </div>
+          <div className="footer-container">
+            <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+            <a
+                className="footer-text"
+                href={TWITTER_LINK}
+                target="_blank"
+                rel="noreferrer"
+            >{`built with @${TWITTER_HANDLE}`}</a>
           </div>
         </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built with @${TWITTER_HANDLE}`}</a>
-        </div>
       </div>
-    </div>
   );
 };
 
---
+++ src/constants.js
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = '0xeD2101c7EF925C72Fc356A6E9c9CD8Bf172163F9';
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+    return {
+      name: characterData.name,
+      imageURI: characterData.imageURI,
+      hp: characterData.hp.toNumber(),
+      maxHp: characterData.maxHp.toNumber(),
+      attackDamage: characterData.attackDamage.toNumber(),
+    };
+  };
+  
+  export { CONTRACT_ADDRESS, transformCharacterData };
